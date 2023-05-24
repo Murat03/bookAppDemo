2f943f0e1d2d0d078f855c82a27637feca4d2b3b
Murat03 <muratipekk033@gmail.com>
2023-05-24 10:06:12 +0300

    .Net Core Web Api Created and Get Method Written

---
+++ bookAppDemo/Controllers/BooksController.cs
@@ -0,0 +1,31 @@
+﻿using bookAppDemo.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bookAppDemo.Controllers
+{
+    [Route("api/books")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllBooks()
+        {
+            var result = ApplicationContext.Books;
+
+            return Ok(result);
+        }
+        [HttpGet("{id:int}")]
+        public IActionResult GetOneBook([FromRoute(Name = "id")]int id)
+        {
+            var result = ApplicationContext.Books.Where(b => b.Id.Equals(id)).SingleOrDefault();
+
+            if(result == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+    }
+}
---
+++ bookAppDemo/Data/ApplicationContext.cs
@@ -0,0 +1,34 @@
+﻿using bookAppDemo.Models;
+
+namespace bookAppDemo.Data
+{
+    public class ApplicationContext
+    {
+        public static List<Book> Books { get; set; }
+        
+        static ApplicationContext()
+        {
+            Books = new List<Book>()
+            {
+                new Book()
+                {
+                    Id = 1,
+                    Title = "Satranç",
+                    Price = 50
+                },
+                new Book()
+                {
+                    Id= 2,
+                    Title = "Utopia",
+                    Price = 100
+                },
+                new Book()
+                {
+                    Id = 3,
+                    Title = "Nasreddin",
+                    Price = 75
+                }
+            };
+        }
+    }
+}
